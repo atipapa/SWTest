a14fa6e4357bf21fb70e36a675463f40a2f6b686
atipapa <atipapa012@gmail.com>
2019-01-29 19:55:26 +0100

    book.java generated hashCode and equals

--- bundle/core/src/main/java/org/core/model/Book.java
+++ bundle/core/src/main/java/org/core/model/Book.java
@@ -20,8 +20,58 @@ throws InvalidPublicationYearException{
 		this.publicationYear = publicationYear;
 		this.ID = ID;
 	}
-public Publisher getPublisher() {
-	return publisher;
-}
-
+	public Publisher getPublisher() {
+		return publisher;
+	}
+	
+	public BookModel getModel() {
+		return model;
+	}
+	
+	public String getWriter() {
+		return writer;
+	}
+	
+	public int getPublicationYear() {
+		return publicationYear;
+	}
+	
+	public int getID() {
+	return ID;
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ID;
+		result = prime * result + ((model == null) ? 0 : model.hashCode());
+		result = prime * result + publicationYear;
+		result = prime * result + ((publisher == null) ? 0 : publisher.hashCode());
+		result = prime * result + ((writer == null) ? 0 : writer.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Book other = (Book) obj;
+		if (ID != other.ID)
+			return false;
+		if (model != other.model)
+			return false;
+		if (publicationYear != other.publicationYear)
+			return false;
+		if (publisher != other.publisher)
+			return false;
+		if (writer == null) {
+			if (other.writer != null)
+				return false;
+		} else if (!writer.equals(other.writer))
+			return false;
+		return true;
+	}
 }
