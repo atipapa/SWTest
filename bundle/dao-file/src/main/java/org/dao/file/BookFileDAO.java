e0d5a7d1a3d6de732b6a06da56bba85b42ebc73e
atipapa <atipapa012@gmail.com>
2019-01-29 21:43:50 +0100

    ???

---
+++ bundle/dao-file/src/main/java/org/dao/file/BookFileDAO.java
@@ -0,0 +1,20 @@
+package org.dao.file;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Collection;
+
+import core.src.main.java.org.core.model.Book;
+
+public class BookFileDAO implements BookDAO{
+
+	public BookFileDAO() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
