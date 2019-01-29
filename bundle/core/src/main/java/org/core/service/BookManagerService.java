cc180a749bfc2de147b97ba09a6b3a937381efaf
atipapa <atipapa012@gmail.com>
2019-01-29 20:49:10 +0100

    throws BookDoesNotExistsException

--- bundle/core/src/main/java/org/core/service/BookManagerService.java
+++ bundle/core/src/main/java/org/core/service/BookManagerService.java
@@ -4,10 +4,11 @@ import java.util.Collection;
 
 import org.core.model.Book;
 import org.core.model.Publisher;
-import org.core.service.exception.BookAlreadyExistsException;;;
+import org.core.service.exception.BookAlreadyExistsException;
+import org.core.service.exception.BookDoesNotExistsException;;;
 
 public interface BookManagerService {
 
 		void recordBook(Book book) throws BookAlreadyExistsException;
-		void updateBook(Book book) throws BookDoes
+		void updateBook(Book book) throws BookDoesNotExistsException;
 }
