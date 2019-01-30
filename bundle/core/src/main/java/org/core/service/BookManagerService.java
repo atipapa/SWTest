2c793aa68d34ccd23d95c7ea9b36e27b961052e2
atipapa <atipapa012@gmail.com>
2019-01-30 23:21:03 +0100

    service

--- bundle/core/src/main/java/org/core/service/BookManagerService.java
+++ bundle/core/src/main/java/org/core/service/BookManagerService.java
@@ -1,11 +1,17 @@
 package org.core.service;
 
+import java.util.Collection;
+
 import org.core.model.Book;
+import org.core.model.Publisher;
 import org.core.service.exception.BookAlreadyExistsException;
-import org.core.service.exception.BookDoesNotExistsException;;;
+import org.core.service.exception.BookDoesNotExistsException;
 
 public interface BookManagerService {
 
 		void recordBook(Book book) throws BookAlreadyExistsException;
 		void updateBook(Book book) throws BookDoesNotExistsException;
+		
+		Collection<Book> listBooks();
+		Collection<Book> listBooksByPublisher(Publisher publisher);
 }
--- bundle/service/src/main/java/org/service/BookManagerServiceImpl.java
+++ bundle/service/src/main/java/org/service/BookManagerServiceImpl.java
@@ -1,13 +1,69 @@
 package org.service;
 
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
+
+import org.core.model.Book;
+import org.core.model.Publisher;
 import org.core.service.BookManagerService;
+import org.core.service.exception.BookAlreadyExistsException;
+import org.core.service.exception.BookDoesNotExistsException;
+import org.dao.BookDAO;
+import org.dao.exception.DuplicatedBookEntryException;
+import org.dao.exception.EntryNotFoundExpcetion;
 
 public class BookManagerServiceImpl implements BookManagerService{
 	
 	private BookDAO bookDAO;
 
 	public BookManagerServiceImpl() {
+		super ();
+		this.bookDAO = bookDAO;
 		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void recordBook(Book book) throws BookAlreadyExistsException {
+		try {
+			bookDAO.createBook(book);
+		} catch (DuplicatedBookEntryException e) {
+			throw new BookAlreadyExistsException(e);
+			// TODO: handle exception
+		}
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void updateBook(Book book) throws BookDoesNotExistsException {
+		try {
+			bookDAO.updateBook(book);
+		} catch (EntryNotFoundExpcetion e) {
+			throw new BookDoesNotExistsException(e);
+			// TODO: handle exception
+		}
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Collection<Book> listBooks() {
+		// TODO Auto-generated method stub
+		return bookDAO.readBooks();
+	}
+
+	@Override
+	public Collection<Book> listBooksByPublisher(Publisher publisher) {
+		Set<Book> result = new HashSet<Book>();
+		for (Book book : listBooks()) {
+			if(book.getPublisher() == publisher) {
+				result.add(book);
+			}
+			
+		}
+		// TODO Auto-generated method stub
+		return result;
 	}
 
 }
